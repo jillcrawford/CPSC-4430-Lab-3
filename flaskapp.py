c318c02cb175fe6c60151a574fd449f3014eff10
jillcrawford <jrcrawford@anderson.edu>
2025-09-19 14:42:14 -0400

    Add route for Rick Astley

--- flaskapp.py
+++ flaskapp.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import send_file, request
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def page():
     return """
     <!doctype html>
@@ -12,7 +12,14 @@ def page():
         <title>CPSC 4430 Lab 3</title>
     </head>
     <body>
-        <img src="rick-astley.jpg" alt="Rick Astley singing 'Never Gonna Give You Up'">
+        <img src="/rick-astley.jpg" alt="Rick Astley singing 'Never Gonna Give You Up'">
     </body>
     </html>
     """
+
+@app.route('/rick-astley.jpg')
+def rick():
+    return send_file('rick-astley.jpg')
+
+if __name__ == '__main__':
+    app.run(debug=True)
