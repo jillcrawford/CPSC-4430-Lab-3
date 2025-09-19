c502cd0e3906866e8edac8d0bb9f6255a90f929d
jillcrawford <jrcrawford@anderson.edu>
2025-09-19 14:25:36 -0400

    Update HTML to meet Flask requirements

--- flaskapp.py
+++ flaskapp.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask import send_file, request
 app = Flask(__name__)
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>CPSC 4430 Lab 3</title>
-  </head>
-  <body>
-    <img src="rick-astley.jpg"></img>
-  </body>
-</html>
+app.route('/')
+def page():
+    return """
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>CPSC 4430 Lab 3</title>
+    </head>
+    <body>
+        <img src="rick-astley.jpg" alt="Rick Astley singing 'Never Gonna Give You Up'">
+    </body>
+    </html>
+    """
